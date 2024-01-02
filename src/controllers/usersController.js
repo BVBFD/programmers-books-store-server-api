f580ee2b1d2d9dca6e222bb2a9c70b9599cfaaec
lsevina126 <lsevina126@gmail.com>
2024-01-02 23:16:37 +0900

    Feat(crypto):crypto 노드 코어 모듈 활용해서 비밀번호 암호화 croptojs 모듈은 폐기

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,17 +1,29 @@
 import conn from "../utils/mariadb.js";
-import CryptoJS from "crypto-js";
+import crypto from "crypto";
+// CryptoJS 사용한 것은 주석처리
+// 서버에서는 추천하지 않음,
+// NodeJS 코어모듈 쓰는것이 보안상 훨씬 더 나음
+// import CryptoJS from "crypto-js";
 import { v4 as uuidv4 } from "uuid";
 import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
 
 const signUp = async (req, res, next) => {
   const { email, password } = req.body;
-  const cryptedPwd = CryptoJS.AES.encrypt(
-    `${password}`,
-    `${process.env.CryptoJS_Secret_Key}`
-  ).toString();
+
+  const salt = crypto.randomBytes(10).toString("base64");
+  const cryptedPwd = crypto
+    .pbkdf2Sync(`${password}`, salt, 10000, 10, "sha512")
+    .toString("base64");
+
+  // const cryptedPwd = CryptoJS.AES.encrypt(
+  //   `${password}`,
+  //   `${process.env.CryptoJS_Secret_Key}`
+  // ).toString();
+
   const uniqueId = uuidv4();
-  const sql = "INSERT INTO users (_id, email, password) VALUES (?, ?, ?)";
+  const sql =
+    "INSERT INTO users (_id, email, password, salt) VALUES (?, ?, ?, ?)";
   let connection;
 
   try {
@@ -23,6 +35,7 @@ const signUp = async (req, res, next) => {
         uniqueId,
         email,
         cryptedPwd,
+        salt,
       ]);
       return res.status(StatusCodes.CREATED).json(results);
       // sql 구문 에러 오류
@@ -65,13 +78,17 @@ const login = async (req, res, next) => {
       } else {
         // 유저 비밀번호 검사, 비밀번호 복호화
         const foundUser = results;
-        const decryptedPwd = CryptoJS.AES.decrypt(
-          `${foundUser.password}`,
-          `${process.env.CryptoJS_Secret_Key}`
-        ).toString(CryptoJS.enc.Utf8);
+        const decryptedPwd = crypto
+          .pbkdf2Sync(`${password}`, foundUser.salt, 10000, 10, "sha512")
+          .toString("base64");
+
+        // const decryptedPwd = CryptoJS.AES.decrypt(
+        //   `${foundUser.password}`,
+        //   `${process.env.CryptoJS_Secret_Key}`
+        // ).toString(CryptoJS.enc.Utf8);
 
         // 원본 비밀번화 복호화된 비밀번호 비교
-        if (decryptedPwd === password.toString()) {
+        if (decryptedPwd === foundUser.password.toString()) {
           // 로그인 성공
           const { _id, email, created_at, updated_at } = foundUser;
           // 토큰 발행
@@ -171,18 +188,28 @@ const passwordResetRequest = async (req, res, next) => {
 
 const passwordReset = async (req, res, next) => {
   const { email: emailBody, password: passwordBody } = req.body;
-  const sql = "UPDATE users SET password=? WHERE email = ?";
+  const sql = "UPDATE users SET password=?, salt=? WHERE email = ?";
   let connection;
-  const cryptedPwd = CryptoJS.AES.encrypt(
-    `${passwordBody}`,
-    `${process.env.CryptoJS_Secret_Key}`
-  ).toString();
+
+  const salt = crypto.randomBytes(10).toString("base64");
+  const cryptedPwd = crypto
+    .pbkdf2Sync(passwordBody, salt, 10000, 10, "sha512")
+    .toString("base64");
+
+  // const cryptedPwd = CryptoJS.AES.encrypt(
+  //   `${passwordBody}`,
+  //   `${process.env.CryptoJS_Secret_Key}`
+  // ).toString();
 
   try {
     connection = conn();
 
     try {
-      const [results] = await connection.query(sql, [cryptedPwd, emailBody]);
+      const [results] = await connection.query(sql, [
+        cryptedPwd,
+        salt,
+        emailBody,
+      ]);
 
       if (results.affectedRows === 1) {
         // 업데이트 성공
