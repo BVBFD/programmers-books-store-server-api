74c44ed2e965e5e773bcf80a1ada0846c4443e85
lsevina126 <lsevina126@gmail.com>
2024-01-10 17:11:33 +0900

    Comment(공부내용):DB 연결에 대해서 공부한 내용 주석공유 및 연결할때 try, catch 문을 활용해서 에러 핸들링도 해보았음

--- src/utils/mariadb.js
+++ src/utils/mariadb.js
@@ -1,16 +1,29 @@
 import mysql from "mysql2";
 
-const connection = () =>
-  mysql
-    .createPool({
-      host: `${process.env.MY_SQL_HOST}` || "localhost",
-      user: `${process.env.MY_SQL_USER}` || "root",
-      //   timezone: "Asia/Seoul",
-      password: `${process.env.MY_SQL_PASSWORD}`,
-      database: `${process.env.MY_SQL_DATABASE}`,
+const connectToDatabase = () => {
+  try {
+    // MySQL 연결 풀 생성
+    const pool = mysql.createPool({
+      host: process.env.MY_SQL_HOST || "localhost",
+      user: process.env.MY_SQL_USER || "root",
+      password: process.env.MY_SQL_PASSWORD,
+      database: process.env.MY_SQL_DATABASE,
       connectionLimit: 10,
       dateStrings: true,
-    })
-    .promise();
+    });
 
-export default connection;
+    // promise() 메서드를 사용하여 비동기 함수로 변환
+    const connection = pool.promise();
+    // 반환된 연결을 사용하여 비동기적으로 쿼리 수행 가능
+
+    // 예시: 비동기적으로 쿼리 수행
+    // const [rows, fields] = await connection.query('SELECT * FROM mytable');
+    return connection;
+  } catch (error) {
+    // 에러 처리
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }
+};
+
+export default connectToDatabase;
